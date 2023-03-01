238984f02146b495c59c2e737eeb65464550f936
priyanshukumar2112 <125110017+priyanshukumar2112@users.noreply.github.com>
2023-03-01 02:08:10 -0800

    completed

--- app/src/main/java/com/rare/taskaletdialogwithlistview/MainActivity.kt
+++ app/src/main/java/com/rare/taskaletdialogwithlistview/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.rare.taskaletdialogwithlistview
 
-import android.app.Dialog
-import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.rare.taskaletdialogwithlistview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var arrayList=ArrayList<String>()
+    lateinit var arrayAdapter: ArrayAdapter<String>
     lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        arrayAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1, arrayList)
+        binding.lv.adapter = arrayAdapter
+
 
         binding.btnfab.setOnClickListener{
         var dialog = AlertDialog.Builder(this)
@@ -23,16 +30,36 @@ class MainActivity : AppCompatActivity() {
                 val length = 3
                 var randomString = getRandomString(length)
                 binding.tvrandomcstr.setText(randomString.toString())
+                arrayList.add(binding.tvrandomcstr.text.toString())
+                arrayAdapter.notifyDataSetChanged()
             }
             dialog.setNegativeButton("4"){_,_ ->
                 val length = 4
                 var randomString = getRandomString(length)
                 binding.tvrandomcstr.setText(randomString.toString())
+                arrayList.add(binding.tvrandomcstr.text.toString())
+                arrayAdapter.notifyDataSetChanged()
             }
             dialog.setPositiveButton("5"){_,_ ->
                 val length = 5
                 var randomString = getRandomString(length)
                 binding.tvrandomcstr.setText(randomString.toString())
+                arrayList.add(binding.tvrandomcstr.text.toString())
+                arrayAdapter.notifyDataSetChanged()
+            }
+            dialog.show()
+        }
+        binding.lv.setOnItemClickListener{ adapterView: AdapterView<*>, view2: View, i: Int, l: Long ->
+            var dialog = android.app.AlertDialog.Builder(this)
+            dialog.setTitle("Delete Alert!!")
+            dialog.setMessage("Are you sure ")
+            dialog.setPositiveButton("yes") { _, _ ->
+                Toast.makeText(this, "Deleted", Toast.LENGTH_SHORT).show()
+                arrayList.removeAt(i)
+                arrayAdapter.notifyDataSetChanged()
+            }
+            dialog.setNegativeButton("No") { _, _ ->
+                Toast.makeText(this, "Operation Cancelled", Toast.LENGTH_SHORT).show()
             }
             dialog.show()
         }
